9d070232d578e01dc66058035bcb10137e9de832
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-03-21 00:07:07 +0900

    add RoomSelect component

--- ReactNative/src/screens/Reservation/ReservationScreen.jsx
+++ ReactNative/src/screens/Reservation/ReservationScreen.jsx
@@ -6,10 +6,12 @@ import {
   ExpandableCalendar, AgendaList, CalendarProvider,
 } from 'react-native-calendars';
 import TimeLine from '../../components/Reservation/TimeLine';
+import RoomSelect from '../../components/Reservation/RoomSelect';
 
 function ReservationScreen({ navigation }) {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <RoomSelect />
       <TimeLine />
     </SafeAreaView>
   );
