7fbe98c4f6bbcda7043e4370295758ec10075f06
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-03-23 01:08:45 +0900

    make main navigation

--- ReactNative/App.jsx
+++ ReactNative/App.jsx
@@ -1,91 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   SafeAreaView, Platform, StatusBar, StyleSheet,
 } from 'react-native';
 import { RecoilRoot } from 'recoil';
-import HistoryScreen from './src/screens/History/HistoryScreen';
-import HomeScreen from './src/screens/Home/HomeScreen';
-import QRScreen from './src/screens/QR/QRScreen';
-import ReservationScreen from './src/screens/Reservation/ReservationScreen';
-import SelectionTimeScreen from './src/screens/Reservation/SelectionTimeScreen';
-import UserScreen from './src/screens/User/UserScreen';
-
-const Tab = createBottomTabNavigator();
-const HomeStack = createNativeStackNavigator();
-const ReservationStack = createNativeStackNavigator();
-const HistoryStack = createNativeStackNavigator();
-const QRStack = createNativeStackNavigator();
-const UserStack = createNativeStackNavigator();
+import MainNavigation from './src/navigation/MainNavigation';
 
 function App() {
   return (
     <RecoilRoot>
       <SafeAreaView style={styles.safeArea}>
         <NavigationContainer>
-          <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Home">
-              {() => (
-                <HomeStack.Navigator>
-                  <HomeStack.Screen
-                    name="HomeScreen"
-                    component={HomeScreen}
-                    options={{ headerShown: false }}
-                  />
-                </HomeStack.Navigator>
-              )}
-            </Tab.Screen>
-            <Tab.Screen name="Reservation">
-              {() => (
-                <ReservationStack.Navigator>
-                  <ReservationStack.Screen
-                    name="ReservationScreen"
-                    component={ReservationScreen}
-                    options={{ headerShown: false }}
-                  />
-                  <ReservationStack.Screen
-                    name="SelectionTimeScreen"
-                    component={SelectionTimeScreen}
-                    options={{ headerShown: false }}
-                  />
-                </ReservationStack.Navigator>
-              )}
-            </Tab.Screen>
-            <Tab.Screen name="History">
-              {() => (
-                <HistoryStack.Navigator>
-                  <HistoryStack.Screen
-                    name="HistoryScreen"
-                    component={HistoryScreen}
-                    options={{ headerShown: false }}
-                  />
-                </HistoryStack.Navigator>
-              )}
-            </Tab.Screen>
-            <Tab.Screen name="QR">
-              {() => (
-                <QRStack.Navigator>
-                  <QRStack.Screen
-                    name="QRScreen"
-                    component={QRScreen}
-                    options={{ headerShown: false }}
-                  />
-                </QRStack.Navigator>
-              )}
-            </Tab.Screen>
-            <Tab.Screen name="User">
-              {() => (
-                <UserStack.Navigator>
-                  <UserStack.Screen
-                    name="UserScreen"
-                    component={UserScreen}
-                    options={{ headerShown: false }}
-                  />
-                </UserStack.Navigator>
-              )}
-            </Tab.Screen>
-          </Tab.Navigator>
+          <MainNavigation />
         </NavigationContainer>
       </SafeAreaView>
     </RecoilRoot>
---
+++ ReactNative/src/navigation/MainNavigation.jsx
@@ -0,0 +1,84 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HistoryScreen from '../screens/History/HistoryScreen';
+import HomeScreen from '../screens/Home/HomeScreen';
+import QRScreen from '../screens/QR/QRScreen';
+import ReservationScreen from '../screens/Reservation/ReservationScreen';
+import SelectionTimeScreen from '../screens/Reservation/SelectionTimeScreen';
+import UserScreen from '../screens/User/UserScreen';
+
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+const ReservationStack = createNativeStackNavigator();
+const HistoryStack = createNativeStackNavigator();
+const QRStack = createNativeStackNavigator();
+const UserStack = createNativeStackNavigator();
+
+function MainNavigation() {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Home">
+        {() => (
+          <HomeStack.Navigator>
+            <HomeStack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+          </HomeStack.Navigator>
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="Reservation">
+        {() => (
+          <ReservationStack.Navigator>
+            <ReservationStack.Screen
+              name="ReservationScreen"
+              component={ReservationScreen}
+              options={{ headerShown: false }}
+            />
+            <ReservationStack.Screen
+              name="SelectionTimeScreen"
+              component={SelectionTimeScreen}
+              options={{ headerShown: false }}
+            />
+          </ReservationStack.Navigator>
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="History">
+        {() => (
+          <HistoryStack.Navigator>
+            <HistoryStack.Screen
+              name="HistoryScreen"
+              component={HistoryScreen}
+              options={{ headerShown: false }}
+            />
+          </HistoryStack.Navigator>
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="QR">
+        {() => (
+          <QRStack.Navigator>
+            <QRStack.Screen
+              name="QRScreen"
+              component={QRScreen}
+              options={{ headerShown: false }}
+            />
+          </QRStack.Navigator>
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="User">
+        {() => (
+          <UserStack.Navigator>
+            <UserStack.Screen
+              name="UserScreen"
+              component={UserScreen}
+              options={{ headerShown: false }}
+            />
+          </UserStack.Navigator>
+        )}
+      </Tab.Screen>
+    </Tab.Navigator>
+  );
+}
+
+export default MainNavigation;
