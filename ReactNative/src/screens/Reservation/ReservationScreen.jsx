377df2ed3f7d63c1c9228c3d1392f1c9b646aa0d
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-03-23 01:10:23 +0900

    clear unnecesarry code

--- ReactNative/src/screens/Reservation/ReservationScreen.jsx
+++ ReactNative/src/screens/Reservation/ReservationScreen.jsx
@@ -2,11 +2,6 @@ import { useState } from 'react';
 import {
   SafeAreaView, View, Text, FlatList, TouchableOpacity, StyleSheet, Alert,
 } from 'react-native';
-import {
-  ExpandableCalendar, AgendaList, CalendarProvider,
-} from 'react-native-calendars';
-import TimeLine from '../../components/Reservation/TimeLine';
-import RoomSelect from '../../components/Reservation/RoomSelect';
 import WideButton from '../../components/Common/WideButton';
 
 function ReservationScreen({ navigation }) {
