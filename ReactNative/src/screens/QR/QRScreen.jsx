c31cae480f2d4bee5bf65a31dfac9688b0fbdc0c
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-03-24 23:27:32 +0900

    add transfer on QR screen

--- ReactNative/src/screens/QR/QRScreen.jsx
+++ ReactNative/src/screens/QR/QRScreen.jsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native';
+import HalfWideButton from '../../components/Common/HalfWideButton';
 
 function QRScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>QR Screen in dir</Text>
+      <HalfWideButton
+        label="Home"
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 }
