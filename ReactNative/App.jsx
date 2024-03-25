86e10a2860f8a264a877a8a3f5c55997c812286a
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-03-25 22:28:27 +0900

    merge AppContext

--- ReactNative/App.jsx
+++ ReactNative/App.jsx
@@ -1,11 +1,58 @@
-import {
-  SafeAreaView, Platform, StyleSheet,
-} from 'react-native';
-import { RecoilRoot, useRecoilValue, useRecoilState } from 'recoil';
+import { NavigationContainer } from '@react-navigation/native';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 import { useState, useEffect } from 'react';
+import {
+  SafeAreaView, Platform, StyleSheet, StatusBar,
+} from 'react-native';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import signInStatus from './src/recoil/signInStatus';
-import AppContent from './AppContext';
+import MainNavigation from './src/navigation/MainNavigation';
+import AuthNavigation from './src/navigation/AuthNavigation';
+import LaunchScreen from './src/screens/Auth/LaunchScreen';
+
+function AppContent() {
+  const isSignedIn = useRecoilValue(signInStatus);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSignInStatus = async () => {
+      try {
+        const signedInStatus = await AsyncStorage.getItem('isSignedIn');
+        if (signedInStatus === 'true') {
+          setUser(true);
+        } else {
+          setUser(false);
+        }
+      } catch (error) {
+        setUser(false);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    checkSignInStatus();
+  }, []);
+
+  useEffect(() => {
+    setUser(isSignedIn);
+  }, [isSignedIn]);
+
+  if (loading) {
+    return (
+      <LaunchScreen />
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <NavigationContainer>
+        {user ? <MainNavigation /> : <AuthNavigation />}
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
 
 function App() {
   return (
@@ -15,3 +62,10 @@ function App() {
   );
 }
 export default App;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
