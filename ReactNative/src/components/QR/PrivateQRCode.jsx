710db3348540d43c5a3609f215438c4fe27e097a
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-04-08 00:28:15 +0900

    add: add qr code

---
+++ ReactNative/src/components/QR/PrivateQRCode.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+
+export default function PrivateQRCode() {
+  return (
+    <View style={styles.container}>
+      <Text>QRコード</Text>
+      <QRCode
+        value='golf-booking-test-private-qrß'
+        size={300}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- ReactNative/src/screens/QR/QRScreen.jsx
+++ ReactNative/src/screens/QR/QRScreen.jsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native';
 import HalfWideButton from '../../components/Common/HalfWideButton';
+import PrivateQRCode from '../../components/QR/PrivateQRCode';
 
 function QRScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>QR Screen in dir</Text>
+      <PrivateQRCode />
       <HalfWideButton
         label="Home"
         onPress={() => navigation.navigate('Home')}
