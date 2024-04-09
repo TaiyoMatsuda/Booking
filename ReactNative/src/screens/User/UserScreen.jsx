e928970a4fdeb1eca4eade6133afba097c29efdb
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-04-09 23:39:39 +0900

    add a card on User screen

--- ReactNative/src/screens/User/UserScreen.jsx
+++ ReactNative/src/screens/User/UserScreen.jsx
@@ -1,9 +1,28 @@
 import { View, Text } from 'react-native';
+import { Button, Card } from 'react-native-paper';
 
 function UserScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: 'left', justifyContent: 'center' }}>
       <Text>User Screen in dir</Text>
+      <View>
+        <Text>・名前</Text>
+        <Text>・TEL</Text>
+        <Text>・住所</Text>
+        <Text>・Email</Text>
+      </View>
+      <Card>
+        <Card.Title title="プラン" />
+        <Card.Content>
+          <Text>プラン1. ¥1000</Text>
+          <Text>残り時間: 10H</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button onPress={() => navigation.navigate('Home')}>
+            プランの変更
+          </Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 }
