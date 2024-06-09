073fb99a9ba5528004f00ed7bd7ea01acff83bfb
TaiyoMatsuda <mtsuda.taiyou@gmail.com>
2024-06-09 21:41:11 +0900

    fix: change splash screen

--- ReactNative/App.jsx
+++ ReactNative/App.jsx
@@ -1,16 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { RecoilRoot, useRecoilValue } from 'recoil';
 import { useState, useEffect } from 'react';
-import {
-  SafeAreaView, Platform, StyleSheet, StatusBar,
-} from 'react-native';
+import { SafeAreaView, Platform, StyleSheet, StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import signInStatus from './src/recoil/signInStatus';
 import MainNavigation from './src/navigation/MainNavigation';
 import AuthNavigation from './src/navigation/AuthNavigation';
 import LaunchScreen from './src/screens/Auth/LaunchScreen';
+
+SplashScreen.preventAutoHideAsync();
 
 function AppContent() {
   const isSignedIn = useRecoilValue(signInStatus);
@@ -20,19 +21,27 @@ function AppContent() {
   useEffect(() => {
     const checkSignInStatus = async () => {
       try {
-        console.log('checkSignInStatus-satrt:isSignedIn' + JSON.stringify(isSignedIn));
+        console.log(
+          'checkSignInStatus-satrt:isSignedIn' + JSON.stringify(isSignedIn),
+        );
         const signedInStatus = await AsyncStorage.getItem('isSignedIn');
-        console.log('checkSignInStatus-end:signedInStatus' + JSON.stringify(signedInStatus));
+        console.log(
+          'checkSignInStatus-end:signedInStatus' +
+            JSON.stringify(signedInStatus),
+        );
 
         if (signedInStatus === 'true') {
           setUser(true);
         } else {
           setUser(false);
         }
+
+        await new Promise(resolve => setTimeout(resolve, 2000));
       } catch (error) {
         setUser(false);
       } finally {
-        setLoading(false);
+        //setLoading(false);
+        SplashScreen.hideAsync();
       }
     };
 
@@ -44,11 +53,9 @@ function AppContent() {
     setUser(isSignedIn);
   }, [isSignedIn]);
 
-  if (loading) {
-    return (
-      <LaunchScreen />
-    );
-  }
+  // if (loading) {
+  //   return <LaunchScreen />;
+  // }
 
   return (
     <SafeAreaView style={styles.safeArea}>
