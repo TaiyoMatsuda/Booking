dbd0714d8d4fa93f57b8d26690b8b44f8cb4d2ca
TaiyoMatsuda <33076874+TaiyoMatsuda@users.noreply.github.com>
2024-06-23 19:54:36 +0900

    fix: tmp fix

--- FastAPI/api/routers/retnal_product.py
+++ FastAPI/api/routers/retnal_product.py
@@ -11,15 +11,14 @@ async def list_rental_products():
 
 
 @router.post("/rental-products", response_model=rental_product_schema.RentalProductCreateResponse)
-async def create_retnal_product(rental_product_body: rental_product_schema.RentalProductCreate):
+async def create_rental_product(rental_product_body: rental_product_schema.RentalProductCreate):
     return rental_product_schema.RentalProductCreateResponse(id=1, **rental_product_body.dict())
 
 
-@router.put("/rental-products/{rental_product_id}")
-async def update_rental_product():
-    pass
+@router.put("/rental-products/{rental_product_id}", response_model=rental_product_schema.RentalProductCreateResponse)
+async def update_rental_product(rental_product_id: int, rental_product_body: rental_product_schema.RentalProductCreate):
+    return rental_product_schema.RentalProductCreateResponse(id=rental_product_id, **rental_product_body.dict())
 
-
-@router.delete("/rental-products/{rental_product_id}")
-async def delete_rental_product():
-    pass
+@router.delete("/rental-products/{rental_product_id}", response_model=None)
+async def delete_rental_product(task_id: int):
+    return
--- FastAPI/api/schemas/retntal_product.py
+++ FastAPI/api/schemas/retntal_product.py
@@ -1,12 +1,11 @@
 from typing import Optional
-
-from pydantic import BaseModel, HttpUrl, Field
+from pydantic import BaseModel, Field
 
 
 class RentalProductBase(BaseModel):
     name: str = Field(..., description="レンタル商品名", max_length=10)
     price: int = Field(..., gt=0, description="値段")
-    image_url: str = Field(None, description="レンタル商品イメージurl")
+    image_url: Optional[str] = Field(None, description="レンタル商品イメージurl")
 
 class RentalProduct(RentalProductBase):
     id: int = Field(..., description="id")
