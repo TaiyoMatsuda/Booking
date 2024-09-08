d1fbe0b99043f7c993c6c9045da9f4fb9f81bebf
TaiyoMatsuda <33076874+TaiyoMatsuda@users.noreply.github.com>
2024-09-08 15:26:10 +0900

    fix: To use debug on local

--- FastAPI/api/db.py
+++ FastAPI/api/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-ASYNC_DB_URL = "mysql+aiomysql://root@db:3306/booking?charset=utf8"
+ASYNC_DB_URL = "mysql+aiomysql://root@localhost:33306/booking?charset=utf8"
 
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
 async_session = sessionmaker(
